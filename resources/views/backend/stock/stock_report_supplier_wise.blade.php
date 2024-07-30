96543f325f8fed6191fc003374dd524610e01646
shoreful-mardy <smbisal6037@gmail.com>
2024-07-30 20:08:16 +0600

    Product wise Report form Design

--- resources/views/backend/stock/stock_report_supplier_wise.blade.php
+++ resources/views/backend/stock/stock_report_supplier_wise.blade.php
@@ -26,6 +26,7 @@
                             <input type="radio" name="supplier_product_wise" value="product_wise" class="search_value">
                         </div>
                     </div><!-- end row -->
+                    <!-- Supplier Wise Report Start -->
                     <div class="show_supplier" style="display: none;">
                        <form method="GET" action="{{ route('supplier.wise.pdf') }}" id="myForm" target="_blank">
                         <div class="row">
@@ -45,6 +46,42 @@
                         </div>
                        </form> 
                     </div>
+
+                    <!-- Supplier Wise Report End -->
+                    <!-- Product Wise Report Start -->
+                    &nbsp;&nbsp;
+                     <div class="show_product" style="display:none ;">
+                       <form method="GET" action="{{ route('supplier.wise.pdf') }}" id="myForm" target="_blank">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="mb-3">
+                                    <label for="example-text-input">Category Name</label>
+                                    <select id="category_id" name="category_id" class="form-select select2" aria-label="Default select example">
+                                            <option selected="">Open this select Category</option>
+                                            @foreach($category as $item)
+                                            <option value="{{ $item->id}}">{{ $item->name}}</option>
+                                            @endforeach
+                                     </select>
+                                 </div>
+                             </div>
+
+                            <div class="col-md-5">
+                                <div class="mb-3">
+                                <label for="example-text-input" >Product Name</label>
+                                <select id="product_id" name="product_id" class="form-select select2" aria-label="Default select example">
+                                    <option value="" >Open this select Product</option>
+                                    
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-3" style="padding-top: 28px;">
+                                <button type="submit" class="btn btn-primary">Search</button>
+                            </div>
+                        </div>
+                       </form> 
+                    </div>
+                    <!-- Product Wise Report End -->
                 </div>
             </div>
         </div> <!-- end col -->
@@ -88,5 +125,36 @@
             $('.show_supplier').hide();
         }
     });
+
+    $(document).on('change','.search_value', function(){
+        var search_value = $(this).val();
+        if (search_value == "product_wise") {
+            $('.show_product').show();
+        }else{
+            $('.show_product').hide();
+        }
+    });
+</script>
+
+<script type="text/javascript">
+    $(function(){
+        $(document).on('change', '#category_id',function() {
+            var category_id = $(this).val();
+            $.ajax({
+                url:"{{ route('get-product') }}",
+                type: "GET",
+                data:{category_id:category_id},
+                success:function(data){
+                    var html = '<option value=""> Select Product </option>';
+                    $.each(data,function(key,v){
+                        html += '<option value=" '+v.id+' ">  ' +v.name+ ' </option>';
+                    });
+
+                    $('#product_id').html(html);
+                }
+            })
+        });
+    });
+    
 </script>
 @endsection
